5f48113f831fc72c5df1b114199db30d7227998a
Artem Zagarov <artem.zagarov@gmail.com>
2021-05-09 15:57:22 +0300

    generalt optimizations and added subscription feature

--- controllers/sub-handler.ts
+++ controllers/sub-handler.ts
@@ -2,8 +2,8 @@ import { subAdd } from './db'
 import Validator from './validator'
 
 export const addSub = (req: any, res: any) => {
-	if (!req.body || !req.body.email) return res.json({ err: 'No data submitted' })
-	if (!Validator.checkString(req.body?.email)) return res.json({ message: 'no email sent' })
+	if (!req.body || !req.body.email) return res.json({ type: 'error' })
+	if (!Validator.checkString(req.body?.email)) return res.json({ type: 'error' })
 	subAdd(req.body.email)
 		.then((r: any) =>
 			res.json({
