873e35c347b26f8c16b0dc1e63cd826a6573be69
Artem Zagarov <artem.zagarov@gmail.com>
2021-05-10 04:22:02 +0300

    added fix to order creation bug, when submitter has not been saved

--- controllers/order-handler.ts
+++ controllers/order-handler.ts
@@ -4,13 +4,15 @@ import Validator from './validator'
 export const create = (req: any, res: any) => {
 	if (!req.body) return res.json({ err: 'No data submitted' })
 	if (!Validator.createOrder(req.body)) return res.json({ message: 'I bet you forgot to mention size :D' })
-	createOrder(req.body)
+	const orderObj = req.body
+	orderObj.submitter = req.user._id
+	createOrder(orderObj)
 		.then((r: any) =>
 			res.json({
 				message: 'Order created!',
 				order: {
 					_id: r._id,
-					submitter: req.user._id,
+					submitter: r.submitter,
 					itemId: r.itemId,
 					size: r.size,
 					color: r.color,
