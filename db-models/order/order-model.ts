83cf2b645aeebcad978553f2c5cd34fde0862530
Artem Zagarov <artem.zagarov@gmail.com>
2021-05-14 22:09:38 +0300

    Changed issuer type to String in Order schema

--- db-models/order/order-model.ts
+++ db-models/order/order-model.ts
@@ -4,7 +4,7 @@ import { IOrderDocument } from '../../types'
 const OrderSchema: Schema = new Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	itemId: mongoose.Schema.Types.ObjectId,
-	submitter: mongoose.Schema.Types.ObjectId || String,
+	submitter: String,
 	size: String,
 	color: String,
 	status: String,
