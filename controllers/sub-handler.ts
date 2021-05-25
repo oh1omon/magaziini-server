83f1262f4685e142eeec3a7dec9caf2f48c99b82
Artem Zagarov <artem.zagarov@gmail.com>
2021-05-25 14:54:38 +0300

    sub-hanlder.ts file is documented and typed

--- controllers/sub-handler.ts
+++ controllers/sub-handler.ts
@@ -1,14 +1,32 @@
+import { Response } from 'express'
+import { Request } from '../types'
 import { subAdd } from './db'
 import Validator from './validator'
 
-export const addSub = (req: any, res: any) => {
-	if (!req.body || !req.body.email) return res.json({ type: 'error' })
-	if (!Validator.checkString(req.body?.email)) return res.json({ type: 'error' })
+/**
+ * Function validates user input and then dispatcher sub add function.
+ * After receiving response from that function we, sending response to the user
+ * @param req Request
+ * @param res Response
+ * @returns {void}
+ */
+export const addSub = (req: Request, res: Response): void => {
+	if (!req.body || !req.body.email) {
+		res.json({ type: 'error' })
+		return
+	}
+	if (!Validator.checkString(req.body?.email)) {
+		res.json({ type: 'error' })
+		return
+	}
 	subAdd(req.body.email)
-		.then((r: any) =>
+		.then(() =>
 			res.json({
 				type: 'info',
 			})
 		)
-		.catch((e) => res.json({ type: 'error' }))
+		.catch((e: Error) => {
+			console.log(e)
+			res.json({ type: 'error' })
+		})
 }
