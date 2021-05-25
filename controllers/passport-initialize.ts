c18277d523da57b6197a4390000d23ed396d5c17
Artem Zagarov <artem.zagarov@gmail.com>
2021-05-25 14:49:31 +0300

    typed and documented passport-initialize.ts

--- controllers/passport-initialize.ts
+++ controllers/passport-initialize.ts
@@ -1,5 +1,8 @@
 import bcrypt from 'bcrypt'
+import { ObjectId } from 'mongoose'
+import { PassportStatic } from 'passport'
 import LocalStrategy from 'passport-local'
+import { IUser, UserDocument } from '../types'
 // const LocalStrategy = require('passport-local').Strategy
 
 /**
@@ -9,35 +12,38 @@ import LocalStrategy from 'passport-local'
  * Intakes passport instance,
  * function to find user by email to let authenticating able,
  * function to find user by id to deserialize the user*/
-export const initializePassport = (passport: any, findOneUser: any) => {
-    const authenticateUser = async (
-        email: string,
-        password: string,
-        done: any
-    ) => {
-        const user = await findOneUser('email', email)
-        if (!user) {
-            return done(null, false, {
-                message: `No user found with ${email} email!`,
-            })
-        }
-        try {
-            if (await bcrypt.compare(password, user.password)) {
-                return done(null, user, { message: 'Authorized!' })
-            } else {
-                return done(null, false, { message: 'Wrong password' })
-            }
-        } catch (e) {
-            return done(e)
-        }
-    }
-    passport.use(
-        new LocalStrategy.Strategy({ usernameField: 'email' }, authenticateUser)
-    )
-    passport.serializeUser((user: any, done: any) => {
-        done(null, user._id)
-    })
-    passport.deserializeUser(async (id: any, done: any) => {
-        return done(null, await findOneUser('_id', id))
-    })
+export const initializePassport = (
+	passport: PassportStatic,
+	findOneUser: (filter: string, value: string | ObjectId) => Promise<UserDocument>
+): void => {
+	const authenticateUser = async (email: string, password: string, done: any): Promise<any> => {
+		const user = await findOneUser('email', email)
+		if (!user) {
+			return done(null, false, {
+				message: `No user found with ${email} email!`,
+			})
+		}
+		try {
+			if (await bcrypt.compare(password, user.password)) {
+				return done(null, user, { message: 'Authorized!' })
+			} else {
+				return done(null, false, { message: 'Wrong password' })
+			}
+		} catch (e) {
+			return done(e)
+		}
+	}
+
+	//Initializing local strategy
+	passport.use(new LocalStrategy.Strategy({ usernameField: 'email' }, authenticateUser))
+
+	//Function for serializing user
+	passport.serializeUser((user: IUser, done: any) => {
+		done(null, user._id)
+	})
+
+	//Function for deserializing user
+	passport.deserializeUser(async (id: string | ObjectId, done: any) => {
+		return done(null, await findOneUser('_id', id))
+	})
 }
